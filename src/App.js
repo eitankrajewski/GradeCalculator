0b1c1096f208ba27a68bcd61ef483b5f450cc162
Eitan Krajewski <eitankrajewski@yahoo.com>
2025-04-14 22:48:39 -0400

    Selection on form works, working on finishing form and then mapping to list assignments

--- src/App.js
+++ src/App.js
@@ -2,14 +2,55 @@ import { useState } from 'react'
 import './App.css'
 
 function AddAssignment() {
-  function handleSubmit() {
-    
+
+  const [assignments, setAssignments] = useState([])
+  const [newAssignment, setNewAssignment] = useState({
+    assignmentGrade: '',
+    assignmentCategory: ''
+  })  
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setAssignments(previousAssignments => [...previousAssignments, newAssignment]);
+    setNewAssignment({
+      assignmentGrade: '',
+      assignmentCategory: ''
+    })
+
+
   }
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setNewAssignment(previousNewAssignments => ({
+      ...previousNewAssignments,
+      [name]: value
+    }));
+  }
+
   return (
     <>
     <form>
-    <div><input type="number" placeholder='Enter assignment/assessment grade'/></div>
-    <div><input type="dropdown" placeholder='Enter grade category'/></div>
+    <div>
+      <input type="number" 
+      placeholder='Enter assignment/assessment grade' 
+      value={newAssignment.assignmentGrade}
+      onChange={handleChange}
+      />
+      
+    </div>
+    <div>
+      <select
+      name="assignmentCategory"
+      value={newAssignment.assignmentCategory}
+      onChange={handleChange}
+      >
+        <option value="">Select an assignment type</option>
+        <option value="Assignment">Assignment</option>
+        <option value="Assessment">Summative Assessment</option>
+        <option value="Other 1">Select an assignment type</option>
+      </select>
+    </div>
     <button type="submit" onClick={handleSubmit}></button>
     </form>
     </>
