656c9d4c443b75c74f4f27a09c2a9342766dc9fe
Eitan Krajewski <eitankrajewski@yahoo.com>
2025-04-17 23:50:28 -0400

    Organized layout of code so far

--- src/App.js
+++ src/App.js
@@ -1,65 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import './components/Assignments'
 
-function AddAssignment() {
-
-  const [assignments, setAssignments] = useState([])
-  const [newAssignment, setNewAssignment] = useState({
-    assignmentGrade: '',
-    assignmentCategory: ''
-  })  
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setAssignments(previousAssignments => [...previousAssignments, newAssignment]);
-    console.log(newAssignment)
-    setNewAssignment({
-      assignmentGrade: '',
-      assignmentCategory: ''
-    })
-
-
-  }
-  function handleChange(e) {
-    const { name, value } = e.target;
-    
-    setNewAssignment(previousNewAssignments => ({
-      ...previousNewAssignments,
-      [name]: value
-    }));
-  }
-
-  return (
-    <>
-    <form>
-    <div>
-      <input 
-      type="number" 
-      min={0}
-      max={100}
-      name="assignmentGrade"
-      placeholder='Enter assignment/assessment grade' 
-      value={newAssignment.assignmentGrade}
-      onChange={handleChange}
-      />
-      
-    </div>
-    <div>
-      <select
-      name="assignmentCategory"
-      value={newAssignment.assignmentCategory}
-      onChange={handleChange}
-      >
-        <option value="">Select an assignment type</option>
-        <option value="Assignment">Assignment</option>
-        <option value="Assessment">Summative Assessment</option>
-      </select>
-    </div>
-    <button type="submit" onClick={handleSubmit}>Add Assignment</button>
-    </form>
-    </>
-  )
-}
 
 function GradeCalculator() {
   const [currentGrade, setCurrentGrade] = useState(0)
@@ -72,6 +14,11 @@ function GradeCalculator() {
         {currentGrade}
       </div>
       <div className="assignments">
+      {/* <ul>
+        assignments.forEach(element => {
+          assignments
+        });
+      </ul> */}
         <AddAssignment>
         </AddAssignment>
       </div>
