a889a3f6b53e21ef6287dcf07a7a2ea127b6ad44
Eitan Krajewski <eitankrajewski@yahoo.com>
2025-04-20 23:28:25 -0400

    Made remove button on all assignments work, worked on incorporating assignment types + weightings, will begin work on grading system tmrw... LOTS OF WORK DONE TDY! :D

--- src/components/AssignmentsForm.js
+++ src/components/AssignmentsForm.js
@@ -2,19 +2,28 @@ import { useState } from "react";
 
 function AssignmentsForm() {
 
+    let [keyNumber, setKeyNumber] = useState(1)
     let [assignments, setAssignments] = useState([])
+    let [categories, setCategories] = useState({
+      category: '',
+      weighting: 0
+    })
     let [newAssignment, setNewAssignment] = useState({
       assignmentGrade: '',
-      assignmentCategory: ''
-    })  
+      assignmentCategory: '',
+      key: keyNumber
+    })
+    let setupFinished = false;  
   
     function handleSubmit(e) {
       e.preventDefault();
       setAssignments(previousAssignments => [...previousAssignments, newAssignment]);
+      setKeyNumber(keyNumber += 1)
       console.log(newAssignment)
       setNewAssignment({
         assignmentGrade: '',
-        assignmentCategory: ''
+        assignmentCategory: '',
+        key: {keyNumber}
       })
   
   
@@ -26,42 +35,62 @@ function AssignmentsForm() {
         [name]: value
       }));
     }
-  
-    return (
-      <>
-      {/* <ul> */}
-        {assignments.map(assignment => (
-        <li>{assignment.assignmentGrade}</li>,
-        <button onClick={() => assignments.filter(assignment)}>Remove</button>
-        ))}
 
-      {/* </ul> */}
+    function handleRemove(assignment) {
+      let filteredAssignments = assignments.filter(otherGrades => otherGrades !== assignment)
+      setAssignments(filteredAssignments)
+      console.log(assignments)
+    }
+
+if(setupFinished == false) {
+  return(
+    <>
+    <form>
+      <button type="submit" onClick={() => setupFinished = true} ></button>
+    </form>
+   </>
+  )
+} else {
+  return (  
+<>
+
+    <div>
+      {assignments.map(assignment => {
+      return(
+          <>
+            <p key={assignment.key}>{assignment.assignmentGrade}</p>
+            <button onClick={() => handleRemove(assignment)}>Remove</button>
+          </>
+        );
+      })}
       <form>
-      <div>
-        <input 
-        type="number" 
-        name="assignmentGrade"
-        placeholder='Enter assignment/assessment grade' 
-        value={newAssignment.assignmentGrade}
-        onChange={handleChange}
-        />
+        <div>
+          <input 
+          type="number" 
+          name="assignmentGrade"
+          placeholder='Enter assignment/assessment grade' 
+          value={newAssignment.assignmentGrade}
+          onChange={handleChange}
+          />
+          
+        </div>
+        <div>
+          <select
+          name="assignmentCategory"
+          value={newAssignment.assignmentCategory}
+          onChange={handleChange}
+          >
+            <option value="">Select an assignment type</option>
+            <option value="Assignment">Assignment</option>
+            <option value="Assessment">Summative Assessment</option>
+          </select>
+        </div>
+        <button type="submit" onClick={handleSubmit}>Add Assignment</button>
+        </form>
+        </div>
         
-      </div>
-      <div>
-        <select
-        name="assignmentCategory"
-        value={newAssignment.assignmentCategory}
-        onChange={handleChange}
-        >
-          <option value="">Select an assignment type</option>
-          <option value="Assignment">Assignment</option>
-          <option value="Assessment">Summative Assessment</option>
-        </select>
-      </div>
-      <button type="submit" onClick={handleSubmit}>Add Assignment</button>
-      </form>
       </>
     )
   }
-
+}
 export default AssignmentsForm;
---
+++ src/components/AssignmentsManager.js
@@ -0,0 +1,21 @@
+// import AssignmentsForm from "./AssignmentsForm";
+// // import { useState } from "react";
+
+
+
+// function AssignmentsManager({assignments, handleRemove}) {
+//     return (
+//         <>
+//         {assignments.map(assignment => {
+//             return(
+//                 <>
+//                     <p key={assignment.key}>{assignment.assignmentGrade}</p>
+//                     <button onClick={() => handleRemove(assignment)}>Remove</button>
+//                 </>
+//             );
+//         })}
+//         </>
+//     )
+// }
+
+// export default AssignmentsManager;
